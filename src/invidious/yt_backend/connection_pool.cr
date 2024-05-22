4125dfb566a3d7f59f82aa1d1303e9650cf03af3
syeopite <syeopite@syeopite.dev>
2024-05-22 11:29:28 -0700

    Use `make_client` instead of calling `HTTP::Client`
    Using `make_client` to create `HTTP::Client`, allows for a simple way to
    easily add logic to all `HTTP::Client` initialized within Invidious.

--- src/invidious/yt_backend/connection_pool.cr
+++ src/invidious/yt_backend/connection_pool.cr
@@ -22,12 +22,8 @@ struct YoutubeConnectionPool
       response = yield conn
     rescue ex
       conn.close
-
-      conn = HTTP::Client.new(url)
-      conn.proxy = make_configured_http_proxy_client() if CONFIG.http_proxy
-      conn.family = CONFIG.force_resolve
+      conn = make_client(url)
       conn.family = Socket::Family::INET if conn.family == Socket::Family::UNSPEC
-      conn.before_request { |r| add_yt_headers(r) } if url.host == "www.youtube.com"
       response = yield conn
     ensure
       pool.release(conn)
@@ -38,31 +34,14 @@ struct YoutubeConnectionPool
 
   private def build_pool
     DB::Pool(HTTP::Client).new(initial_pool_size: 0, max_pool_size: capacity, max_idle_pool_size: capacity, checkout_timeout: timeout) do
-      conn = HTTP::Client.new(url)
-      conn.family = CONFIG.force_resolve
+      conn = make_client(url, force_solve = true)
       conn.family = Socket::Family::INET if conn.family == Socket::Family::UNSPEC
-      conn.before_request { |r| add_yt_headers(r) } if url.host == "www.youtube.com"
       conn
     end
   end
 end
 
-def add_yt_headers(request)
-  request.headers.delete("User-Agent") if request.headers["User-Agent"] == "Crystal"
-  request.headers["User-Agent"] ||= "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
-
-  request.headers["Accept-Charset"] ||= "ISO-8859-1,utf-8;q=0.7,*;q=0.7"
-  request.headers["Accept"] ||= "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
-  request.headers["Accept-Language"] ||= "en-us,en;q=0.5"
-
-  # Preserve original cookies and add new YT consent cookie for EU servers
-  request.headers["Cookie"] = "#{request.headers["cookie"]?}; CONSENT=PENDING+#{Random.rand(100..999)}"
-  if !CONFIG.cookies.empty?
-    request.headers["Cookie"] = "#{(CONFIG.cookies.map { |c| "#{c.name}=#{c.value}" }).join("; ")}; #{request.headers["cookie"]?}"
-  end
-end
-
-def make_client(url : URI, region = nil, force_resolve : Bool = false)
+def make_client(url : URI, region = nil, force_resolve : Bool = false, force_youtube_header : Bool = false)
   client = HTTP::Client.new(url)
 
   # Force the usage of a specific configured IP Family
@@ -70,7 +49,7 @@ def make_client(url : URI, region = nil, force_resolve : Bool = false)
     client.family = CONFIG.force_resolve
   end
 
-  client.before_request { |r| add_yt_headers(r) } if url.host == "www.youtube.com"
+  client.before_request { |r| add_yt_headers(r) } if url.host == "www.youtube.com" || force_youtube_header
   client.read_timeout = 10.seconds
   client.connect_timeout = 10.seconds
 
